59163d8083b833aaf1ceed2569e0203995297483
Nurul Mustofa <nioke8090@gmail.com>
2024-01-19 15:44:48 +0700

    ci cd test

--- src/knexfile.ts
+++ src/knexfile.ts
@@ -7,7 +7,6 @@ import dotenv from "dotenv";
 
 dotenv.config({ path: path.resolve(__dirname, "../.env") });
 console.log(process.env["DATABASE_URL"]);
-alert(process.env["DATABASE_URL"]);
 
 const config: { [key: string]: Knex.Config } = {
   development: {
